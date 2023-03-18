3c9099c8887ae0facd21dcc6105978e694624160
Solomon Asare <solomonasare0551@gmail.com>
2023-03-18 07:45:51 -0700

    alphabet_in_lowecase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphebet in lowercase
+ * Return: Done (success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
