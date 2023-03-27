431bb1c1befd6cb3da810ce14efe5d6f80b4cd0f
Solomon Asare <solomonasare0551@gmail.com>
2023-03-27 16:41:01 -0700

    funtion that swap values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * swp_int - Swaps the values of two integers.
+ * swap_int - Swaps the values of two integers.
  * @a: The first integer to be swapped.
  * @b: The second integer to be swapped.
  *
